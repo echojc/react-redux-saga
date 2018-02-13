2f08ef4094855640c8ba8ad0c28894804c9d0192
Jonathan Chow <jonathan@movio.co>
2018-02-13 19:50:08 +1300

    Show rendering state to view

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+const todos = [
+  'bootstrap a single page app',
+  'learn about react',
+  'encroach upon redux territory',
+  'tell of epics and sagas',
+];
+
 export default class TodoList extends React.Component {
   render() {
-    return <div>hi there</div>;
+    return (
+      <div>
+        <h2>Todos</h2>
+        <ul>
+          {todos.map(todo => <li key={todo}>{todo}</li>)}
+        </ul>
+      </div>
+    );
   }
 }
