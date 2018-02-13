5b30d2458b49e92151bb258d503bebc6c8ea07df
Jonathan Chow <jonathan@movio.co>
2018-02-13 20:06:24 +1300

    Add done

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -5,10 +5,26 @@ export default class TodoList extends React.Component {
     super(props);
     this.state = {
       todos: [
-        'bootstrap a single page app',
-        'learn about react',
-        'encroach upon redux territory',
-        'tell of epics and sagas',
+        {
+          id: 1,
+          label: 'bootstrap a single page app',
+          done: true,
+        },
+        {
+          id: 2,
+          label: 'learn about react',
+          done: false,
+        },
+        {
+          id: 3,
+          label: 'encroach upon redux territory',
+          done: false,
+        },
+        {
+          id: 4,
+          label: 'tell of epics and sagas',
+          done: false,
+        }
       ],
     };
   }
@@ -21,6 +37,17 @@ export default class TodoList extends React.Component {
     this.setState({ todos: this.state.todos.filter(t => t !== todo) });
   }
 
+  toggleTodo(id) {
+    const { todos } = this.state;
+    const index = todos.findIndex(t => t.id === id);
+    const todo = todos[index];
+    const updated = {
+      ...todo,
+      done: !todo.done,
+    };
+    this.setState({ todos: [].concat(todos.slice(0, index), updated, todos.slice(index + 1)) });
+  }
+
   render() {
     return (
       <div>
@@ -28,9 +55,16 @@ export default class TodoList extends React.Component {
         <ul>
           {this.state.todos.map(todo => (
           <li
-            key={todo}
+            key={todo.id}
           >
-            <label><input type="checkbox" />{todo}</label>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.done}
+                onChange={() => this.toggleTodo(todo.id)}
+              />
+              {todo.label}
+            </label>
             <span onClick={() => this.deleteTodo(todo)}>[X]</span>
           </li>
           ))}
