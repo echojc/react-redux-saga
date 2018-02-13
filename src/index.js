1d1949f22a82ff7ea999c7a9baefcf72e9f560ab
Jonathan Chow <me@echo.sh>
2018-02-13 20:51:38 +1300

    Add basic redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import TodoList from './todolist';
+import reducer from './reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <TodoList />,
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
   document.getElementById('root')
 );
