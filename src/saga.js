89f9935519156fa5898e81bded9375e7459aa9dc
Jonathan Chow <me@echo.sh>
2018-02-23 11:58:30 +1300

    Use takeEvery helper

--- src/saga.js
+++ src/saga.js
@@ -1,24 +1,21 @@
-import { call, put, take, all } from 'redux-saga/effects';
+import { call, put, all, takeEvery } from 'redux-saga/effects';
 
 function* loadState() {
   const res = yield call(fetch, 'http://localhost:8899/');
   yield put({ type: 'SET_TODOS', todos: yield res.json() });
 }
 
-function* addTodo() {
-  const action = yield take('ADD_TODO');
+function* addTodo(action) {
   const res = yield call(fetch, 'http://localhost:8899/add', { method: 'POST', body: action.label });
   yield put({ type: 'SET_TODOS', todos: yield res.json() });
 }
 
-function* deleteTodo() {
-  const action = yield take('DELETE_TODO');
+function* deleteTodo(action) {
   const res = yield call(fetch, 'http://localhost:8899/delete', { method: 'POST', body: action.id });
   yield put({ type: 'SET_TODOS', todos: yield res.json() });
 }
 
-function* toggleTodo() {
-  const action = yield take('TOGGLE_TODO');
+function* toggleTodo(action) {
   const res = yield call(fetch, 'http://localhost:8899/toggle', { method: 'POST', body: action.id });
   yield put({ type: 'SET_TODOS', todos: yield res.json() });
 }
@@ -26,8 +23,8 @@ function* toggleTodo() {
 export default function*() {
   yield all([
     loadState(),
-    addTodo(),
-    deleteTodo(),
-    toggleTodo(),
+    takeEvery('ADD_TODO', addTodo),
+    takeEvery('DELETE_TODO', deleteTodo),
+    takeEvery('TOGGLE_TODO', toggleTodo),
   ]);
 }
