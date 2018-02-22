e71e43e3d859474f61f2c613034d885902bea055
Jonathan Chow <me@echo.sh>
2018-02-23 11:55:23 +1300

    Handle other actions too

--- src/saga.js
+++ src/saga.js
@@ -11,9 +11,23 @@ function* addTodo() {
   yield put({ type: 'SET_TODOS', todos: yield res.json() });
 }
 
+function* deleteTodo() {
+  const action = yield take('DELETE_TODO');
+  const res = yield call(fetch, 'http://localhost:8899/delete', { method: 'POST', body: action.id });
+  yield put({ type: 'SET_TODOS', todos: yield res.json() });
+}
+
+function* toggleTodo() {
+  const action = yield take('TOGGLE_TODO');
+  const res = yield call(fetch, 'http://localhost:8899/toggle', { method: 'POST', body: action.id });
+  yield put({ type: 'SET_TODOS', todos: yield res.json() });
+}
+
 export default function*() {
   yield all([
     loadState(),
     addTodo(),
+    deleteTodo(),
+    toggleTodo(),
   ]);
 }
