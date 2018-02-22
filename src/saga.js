a1b3d7f929e504ebbe3d6bff0dc81f56ecc3f4ec
Jonathan Chow <me@echo.sh>
2018-02-23 11:53:57 +1300

    Siphon off add action and talk to backend

--- src/saga.js
+++ src/saga.js
@@ -1,6 +1,19 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, take, all } from 'redux-saga/effects';
 
-export default function* loadState() {
+function* loadState() {
   const res = yield call(fetch, 'http://localhost:8899/');
   yield put({ type: 'SET_TODOS', todos: yield res.json() });
+}
+
+function* addTodo() {
+  const action = yield take('ADD_TODO');
+  const res = yield call(fetch, 'http://localhost:8899/add', { method: 'POST', body: action.label });
+  yield put({ type: 'SET_TODOS', todos: yield res.json() });
+}
+
+export default function*() {
+  yield all([
+    loadState(),
+    addTodo(),
+  ]);
 }
